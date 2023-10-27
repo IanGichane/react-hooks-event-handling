28a3ff71254bf8158c2218895a712378d33bfcdb
IanGichane <gichaneian@gmail.com>
2023-10-27 15:32:12 +0300

    Event handling

--- src/components/ChangeItUp.js
+++ src/components/ChangeItUp.js
@@ -2,7 +2,9 @@ import React from "react";
 
 function ChangeItUp() {
   function handleChange(event) {
-    console.log(`${event.target.name}: ${event.target.value}`);
+    console.log(
+      `${event.target.name}:${event.target.type}: ${event.target.value}`
+    );
   }
 
   return (
